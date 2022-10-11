974b1e769aa54c21470169e61efd382427e2c268
foysalimtiaj <87001955+foysalimtiaj@users.noreply.github.com>
2022-10-11 15:27:04 +0600

    f

--- java.js
+++ java.js
@@ -1,10 +1,11 @@
-function barsClick() {
-    const x=document.getElementById("last");
-    if (x.className==="drop") {
-        x.className+=" try"
-        
+
+const bars=document.getElementById("barss").addEventListener("click", function () {
+    const x=document.getElementById("drop");
+    if (x.className==="dropp") {
+        x.className+=" try";
     }
     else{
-        x.className="drop"
+        x.className="dropp";
     }
-}
+    
+})
